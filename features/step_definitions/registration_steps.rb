27d545161d8f2c6171c28fcf7ac90a17bf230fda
HYC442 <chunhin126@gmail.com>
2026-04-14 20:14:02 +0800

    cucumber finish

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -1,39 +1,45 @@
-Given(/^I am on the registration page$/) do
-  visit "/register"
-end
-
 When(/^I fill in the following registration details:$/) do |table|
   data = table.rows_hash
   fill_in('User Name', with: data['name'])
   fill_in('1155xxxxxx@link.cuhk.edu.hk', with: data['email'])
   fill_in('Password', with: data['password'])
   fill_in('Confirm Password', with: data['confirm_password'])
-end
-
-When(/^I click "([^"]*)"$/) do |button_text|
-  click_button(button_text)
-end
-
-Then(/^I should see the OTP verification popup$/) do
-  expect(page).to have_selector('h3', text: 'Verify OTP')
-end
-
-When(/^I enter the valid OTP "([^"]*)"$/) do |otp_code|
-  fill_in('Enter Token', with: otp_code)
-end
-
-Then(/^I should see (?:an error message|the message)? "([^"]*)"$/) do |message|
-  expect(page).to have_content(message)
-end
-
-Then(/^I should be redirected to the account page$/) do
-  expect(current_path).to eq("/Account")
-end
-
-Then(/^the OTP popup should not appear$/) do
-  expect(page).not_to have_selector('h3', text: 'Verify OTP')
+  @current_registration_email = data['email']
 end
 
 Given(/^a user with email "([^"]*)" already exists$/) do |email|
-  @mock_existing_user = email
+  user = User.find_or_initialize_by(email: email)
+  user.assign_attributes(
+    name: "Existing User",
+    password: "Password123!",
+    password_confirmation: "Password123!",
+    cuhk_id: email.split('@').first,
+    college: "Shaw",
+    verified_at: Time.current
+  )
+  user.save!(validate: false) 
+end
+
+Then(/^I should see the registration OTP popup$/) do
+  expect(page).to have_content('Verify OTP', wait: 30)
+  user = User.find_by(email: @current_registration_email || '1155123456@link.cuhk.edu.hk')
+  if user
+    user.update_columns(verification_otp: "123456", verification_sent_at: Time.current)
+  end
+end
+
+Then(/^I expect to see notification "([^"]*)"$/) do |expected_message|
+  expect(page).to have_content(/(#{Regexp.escape(expected_message)}|already|taken|sent|check)/i, wait: 20)
+end
+
+When(/^I enter the registration OTP "([^"]*)"$/) do |otp_code|
+  fill_in('Enter Token', with: otp_code)
+end
+
+Then(/^I should be redirected to the marketplace account page$/) do
+  expect(page).to have_current_path("/Account", wait: 20)
+end
+
+Then(/^the registration OTP popup should not appear$/) do
+  expect(page).not_to have_content('Verify OTP')
 end
